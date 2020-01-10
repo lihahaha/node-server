cf5634192796f8b9184e1117867483812a2c1472
Neal <704683538@qq.com>
2020-01-10 12:46:35 +0800

    feat: 5

--- app.js
+++ app.js
@@ -10,44 +10,44 @@ const InitManager = require('./core/init')
 const catchError = require('./middlewares/exception')
 
 // error handler
-onerror(app)
+// onerror(app)
 
-// logger
-app.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
-    console.log(`${ctx}`)
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// // logger
+// app.use(async (ctx, next) => {
+//     const start = new Date()
+//     await next()
+//     const ms = new Date() - start
+//     console.log(`${ctx}`)
+//     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
-app.keys = ['Porschev'];
-const redis_conf = {  
-    key: 'Porschev',
-    maxAge: config.REDIS.maxAge,
-    overwrite: true,
-    httpOnly: true,  
-    rolling: false,
-    sign: true,
-    store: new RedisStore({
-        host: config.REDIS.host,
-        port: config.REDIS.port,    
-        password: config.REDIS.password    
-    })
-};
+// app.keys = ['Porschev'];
+// const redis_conf = {  
+//     key: 'Porschev',
+//     maxAge: config.REDIS.maxAge,
+//     overwrite: true,
+//     httpOnly: true,  
+//     rolling: false,
+//     sign: true,
+//     store: new RedisStore({
+//         host: config.REDIS.host,
+//         port: config.REDIS.port,    
+//         password: config.REDIS.password    
+//     })
+// };
 
-app.use(session(redis_conf, app));
+// app.use(session(redis_conf, app));
 
-// error-handling
-app.on('error', (err, ctx) => {
-    // console.error('server error', err, ctx)
-});
+// // error-handling
+// app.on('error', (err, ctx) => {
+//     // console.error('server error', err, ctx)
+// });
 
-// 跨域
-app.use(cors());
-app.use(catchError)
-app.use(parser())
-InitManager.initCore(app)
+// // 跨域
+// app.use(cors());
+// app.use(catchError)
+// app.use(parser())
+// InitManager.initCore(app)
 app.get('/', function (req, res) { 
     res.send('Helloworld\n');
 });
